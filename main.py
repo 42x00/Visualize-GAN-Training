8b0e9d0b2576ae68a242e1c3d7fd9fec7240db75
LYK_Apex <li.yi.kai1998@qq.com>
2018-09-15 11:22:07 +0800

    Add gitignore

--- main.py
+++ main.py
@@ -8,23 +8,24 @@ import pylab as pl
 X_dim = 2
 z_dim = 2
 h_dim = 512
-D_layers = 6
-G_layers = 4
+D_layers = 7
+G_layers = 5
 
 # -- WGAN parameter -- #
 cnt_point = 10
 noise_min = -1.
 noise_max = 1.
-iter_G = 50
-iter_D = 20
-D_learning_rate = 1e-1
+iter_G = 100
+iter_D = 120
+D_learning_rate = 1e-5
 G_learning_rate = 1e-3
-lam = 30
+lam = 0.1
 
 # -- plot parameter -- #
-visual_delay = 0.6
+visual_delay = 0.01
 fig3D = plt.figure(1)
 fig2D = plt.figure(2)
+figloss = plt.figure(3)
 ax = Axes3D(fig3D)
 cnt_interval = 80
 # plot arrange
@@ -54,7 +55,7 @@ def plot_surface_nn(x, y, value, real_point, real_value, fake_point, fake_value,
         plt.title('3D View of ' + str(iter) + ' Iter')
 
         # draw surface
-        ax.plot_surface(x, y, z, rstride=1, cstride=1, cmap='coolwarm', alpha=0.5)
+        ax.plot_surface(x, y, z, rstride=1, cstride=1, cmap='coolwarm', alpha=0.7)
 
         # draw points
         ax.scatter(real_point[:, 0], real_point[:, 1], real_value, color='#D0252D')
@@ -90,6 +91,44 @@ def plot_surface_nn(x, y, value, real_point, real_value, fake_point, fake_value,
     plt.ylim(y_axis_min, y_axis_max)
 
     plt.pause(visual_delay)
+
+
+def plot_loss_change(iter, D_fake_loss, D_real_loss, grad_loss):
+    # add data to history
+    D_fake_loss_rec.append(D_fake_loss)
+    D_real_loss_rec.append(-D_real_loss)
+    GAN_loss_rec.append(-(D_fake_loss - D_real_loss))
+    grad_loss_rec.append(grad_loss)
+
+    with plt.style.context("seaborn-whitegrid"):
+        # -- draw stacked plot -- #
+        pl.figure(3)
+        plt.cla()
+        plt.title('Loss Stack View')
+
+        # draw loss change proportion
+        draw_labels = ['GAN', 'Grad']
+        draw_pal = ['gold', 'dimgray']
+        plt.stackplot(range(max(0, iter - iter_D), iter), GAN_loss_rec[max(0, iter - iter_D): iter],
+                      grad_loss_rec[max(0, iter - iter_D): iter], labels=draw_labels,
+                      colors=draw_pal, alpha=0.7)
+        plt.legend(loc=2)
+
+        # -- draw fake and real expect -- #
+        pl.figure(4)
+        plt.cla()
+        plt.title('Value Expect View')
+
+        # darw value expect
+        plt.fill_between(range(max(0, iter - iter_D), iter), 0, D_fake_loss_rec[max(0, iter - iter_D): iter],
+                         color='#1057AA', alpha=0.7)
+
+        plt.fill_between(range(max(0, iter - iter_D), iter), 0, D_real_loss_rec[max(0, iter - iter_D): iter],
+                         color='#D0252D', alpha=0.7)
+
+        plt.legend(labels=['Fake', 'Real'], loc=2)
+
+        plt.pause(visual_delay)
 
 
 # initialize nn weights
@@ -186,12 +225,15 @@ D_fake = discriminator(G_sample)
 Grad_fake = tf.gradients(D_value, X_toView)
 
 eps = tf.random_uniform([cnt_point, 1], minval=0., maxval=1.)
-X_inter = eps*X + (1. - eps)*G_sample
+X_inter = eps * X + (1. - eps) * G_sample
 grad = tf.gradients(discriminator(X_inter), [X_inter])[0]
-grad_norm = tf.sqrt(tf.reduce_sum((grad)**2, axis=1))
-grad_pen = lam * tf.reduce_mean((grad_norm - 1)**2)
+grad_norm = tf.sqrt(tf.reduce_sum((grad) ** 2, axis=1))
+grad_pen = lam * tf.reduce_mean((grad_norm - 1) ** 2)
 
-D_loss = tf.reduce_mean(D_fake) - tf.reduce_mean(D_real) + grad_pen
+D_fake_mean = tf.reduce_mean(D_fake)
+D_real_mean = tf.reduce_mean(D_real)
+
+D_loss = D_fake_mean - D_real_mean + grad_pen
 G_loss = -tf.reduce_mean(D_fake)
 
 D_solver = (tf.train.AdamOptimizer(learning_rate=D_learning_rate)
@@ -205,14 +247,18 @@ sess.run(tf.global_variables_initializer())
 # -- prepare data -- #
 X_real = sample_z(cnt_point, X_dim)
 z_fix = sample_z(cnt_point, X_dim)
+D_real_loss_rec = []
+D_fake_loss_rec = []
+GAN_loss_rec = []
+grad_loss_rec = []
 
 for iter_g in range(iter_G):
     # train D
     # z_fix = sample_z(cnt_point, X_dim)
     X_fake = sess.run(G_sample, feed_dict={z: z_fix})
     for iter_d in range(iter_D):
-        _, D_loss_curr = sess.run(
-            [D_solver, D_loss],
+        _, D_loss_curr, D_fake_mean_curr, D_real_mean_curr, grad_pen_curr = sess.run(
+            [D_solver, D_loss, D_fake_mean, D_real_mean, grad_pen],
             feed_dict={X: X_real, z: z_fix}
             # feed_dict={X: X_real, z: sample_z(cnt_point, z_dim)}
         )
@@ -222,7 +268,9 @@ for iter_g in range(iter_G):
         Real_value_visual = sess.run(D_value, feed_dict={X_toView: X_real})
         Fake_value_visual = sess.run(D_value, feed_dict={X_toView: X_fake})
         Grad_visual = sess.run(Grad_fake, feed_dict={X_toView: X_fake})
-        plot_surface_nn(x1, x2, Value_visual, X_real, Real_value_visual, X_fake, Fake_value_visual, Grad_visual[0], iter_d)
+        plot_surface_nn(x1, x2, Value_visual, X_real, Real_value_visual, X_fake, Fake_value_visual, Grad_visual[0],
+                        iter_d)
+        plot_loss_change(iter_g * iter_D + iter_d + 1, D_fake_mean_curr, D_real_mean_curr, grad_pen_curr)
 
     # update G
     _, G_loss_curr = sess.run(
